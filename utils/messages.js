fad1f682bba3850ac9dc765f4660de57b4880983
myname <vs20012001vs@gmail.com>
2022-04-25 01:49:46 +0530

    Time

--- utils/messages.js
+++ utils/messages.js
@@ -1,9 +1,12 @@
 const moment = require('moment');
+var today = new Date();
+var time = today.getHours() + ":" + today.getMinutes();
+
 function formatMessage(user,text){
     return {
         user,
         text,
-        time:moment().format('h:mm a')
+        time:time//moment().format('h:mm a')
     }
 };
 
